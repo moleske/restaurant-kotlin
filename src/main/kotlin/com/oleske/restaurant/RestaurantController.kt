333163ac21f4e86d8a0fe1d0ffdb36f88ecd6af2
M. Oleske <moleske@gmail.com>
2017-05-31 15:35:27 -0700

    add get endpoint

--- src/main/kotlin/com/oleske/restaurant/RestaurantController.kt
+++ src/main/kotlin/com/oleske/restaurant/RestaurantController.kt
@@ -2,6 +2,7 @@ package com.oleske.restaurant
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,4 +13,7 @@ class RestaurantController(val restaurantRepository: RestaurantRepository) {
     @PostMapping("/newRestaurant")
     fun createRestaurant(@RequestBody restaurant: Restaurant) =
             ResponseEntity(restaurantRepository.save(restaurant), HttpStatus.CREATED)
+
+    @GetMapping("restaurant")
+    fun getRestaurants() = ResponseEntity(restaurantRepository.findAll(), HttpStatus.OK)
 }
--- src/test/kotlin/com/oleske/restaurant/RestaurantControllerTest.kt
+++ src/test/kotlin/com/oleske/restaurant/RestaurantControllerTest.kt
@@ -2,10 +2,12 @@ package com.oleske.restaurant
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.damo.aspen.Test
+import org.hamcrest.Matchers.hasSize
 import org.mockito.Matchers
 import org.mockito.Mockito.*
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -16,16 +18,15 @@ class RestaurantControllerTest : Test() {
     lateinit var mvc: MockMvc
     lateinit var mockRestaurantRepository: RestaurantRepository
     val objectMapper = ObjectMapper()
+    lateinit var restaurant: Restaurant
 
     init {
         before {
             mockRestaurantRepository = mock(RestaurantRepository::class.java)
             mvc = MockMvcBuilders.standaloneSetup(RestaurantController(mockRestaurantRepository)).build()
-        }
 
-        test("create returns 201") {
-            val request = Restaurant(
-                    id = null,
+            restaurant = Restaurant(
+                    id = 1L,
                     name = "name",
                     ownerName = "ownerName",
                     headChefName = "headChefName",
@@ -37,9 +38,11 @@ class RestaurantControllerTest : Test() {
                     michelinStarRating = 1,
                     zagatRating = 2
             )
+        }
 
-            val restaurant = Restaurant(
-                    id = 1L,
+        test("create returns 201") {
+            val request = Restaurant(
+                    id = null,
                     name = "name",
                     ownerName = "ownerName",
                     headChefName = "headChefName",
@@ -69,7 +72,29 @@ class RestaurantControllerTest : Test() {
                     .andExpect(jsonPath("$.rating").value(request.rating))
                     .andExpect(jsonPath("$.michelinStarRating").value(request.michelinStarRating))
                     .andExpect(jsonPath("$.zagatRating").value(request.zagatRating))
+
             verify(mockRestaurantRepository).save(request)
+        }
+
+        test("get returns data") {
+            `when`(mockRestaurantRepository.findAll()).thenReturn(mutableListOf(restaurant))
+
+            mvc.perform(get("/restaurant"))
+                    .andExpect(status().isOk)
+                    .andExpect(jsonPath("$", hasSize<Any>(1)))
+                    .andExpect(jsonPath("$[0].id").value("1"))
+                    .andExpect(jsonPath("$[0].name").value(restaurant.name))
+                    .andExpect(jsonPath("$[0].ownerName").value(restaurant.ownerName))
+                    .andExpect(jsonPath("$[0].headChefName").value(restaurant.headChefName))
+                    .andExpect(jsonPath("$[0].cuisineType").value(restaurant.cuisineType))
+                    .andExpect(jsonPath("$[0].shortDescription").value(restaurant.shortDescription))
+                    .andExpect(jsonPath("$[0].fullDescription").value(restaurant.fullDescription))
+                    .andExpect(jsonPath("$[0].websiteUrl").value(restaurant.websiteUrl))
+                    .andExpect(jsonPath("$[0].rating").value(restaurant.rating))
+                    .andExpect(jsonPath("$[0].michelinStarRating").value(restaurant.michelinStarRating))
+                    .andExpect(jsonPath("$[0].zagatRating").value(restaurant.zagatRating))
+
+            verify(mockRestaurantRepository).findAll()
         }
     }
 }
